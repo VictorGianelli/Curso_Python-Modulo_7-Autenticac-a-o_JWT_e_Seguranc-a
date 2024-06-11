0e43bbd02d474f23cc5161c45c15a08cadd4ee8d
VictorGianelli <victorgnll@gmail.com>
2024-06-11 13:21:08 -0300

    Jwt Na Pratica - parte 2

--- example_jwt.py
+++ example_jwt.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import jwt
 from datetime import datetime, timedelta, timezone
 
@@ -9,6 +9,7 @@ def login():
   token = jwt.encode(
       payload={
         "exp": datetime.now(timezone.utc) + timedelta(minutes=1),
+        "email": "teste@mail.com"
         },
       key="minhaChave",
       algorithm="HS256"
@@ -17,7 +18,17 @@ def login():
 
 @app.route("/secret", methods=["POST"])
 def secret():
+  raw_token = request.headers.get("Authorization")
+  token = raw_token.split(" ")[1]
+
+  try:
+    token_information = jwt.decode(token, key="minhaChave", algorithms=["HS256"])
+    print(token_information)
+    print(token_information["email"])
+  except Exception as exception:
+    return jsonify({"erro": str(exception)}), 401
+  
   return jsonify({"meu": "segredo"}),200
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=3000)
+  app.run(host="0.0.0.0", port=3000, debug=True)
