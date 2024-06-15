7cf0c4d35b007b1f9f6022f49482e194506ec9ec
VictorGianelli <victorgnll@gmail.com>
2024-06-15 10:37:59 -0300

    Testando Lógica de Registrador de Usuários

---
+++ src/controllers/user_register_test.py
@@ -0,0 +1,28 @@
+from .user_register import UserRegister
+
+class MockUserRepository:
+    def __init__(self) -> None:
+        self.registry_user_attributes = {}
+
+    def registry_user(self, username, password) -> None:
+        self.registry_user_attributes["username"] = username
+        self.registry_user_attributes["password"] = password
+
+
+def test_registry():
+    repository = MockUserRepository()
+    controller = UserRegister(repository)
+
+    username = "olaMundo"
+    password = "myPassword"
+
+    response = controller.registry(username, password)
+    # print("\n", response)
+    # print(repository.registry_user_attributes)
+
+    assert response["type"] == "User"
+    assert response["username"] == username
+
+    assert repository.registry_user_attributes["username"] == username
+    assert repository.registry_user_attributes["password"] is not None
+    assert repository.registry_user_attributes["password"] != password
