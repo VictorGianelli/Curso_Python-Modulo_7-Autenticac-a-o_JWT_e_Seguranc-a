ce8c87797404dc7bccfb126370f2b3169bb80fe6
VictorGianelli <victorgnll@gmail.com>
2024-06-24 18:59:47 -0300

    Interfaces de Controllers

--- src/controllers/balance_editor_test.py
+++ src/controllers/balance_editor_test.py
@@ -13,13 +13,8 @@ def test_balance_editor():
     balance_editor = BalanceEditor(MockBalanceRepository())
     response = balance_editor.edit(1, 1000.0)
 
-    print(response)
+    # print(response)
 
     assert response["type"] == "User"
     assert response["count"] == 1
     assert response["new_balance"] == 1000.0
-#   assert balance_editor.edit(1, 1000.0) == {
-#     "type": "User",
-#     "count": 1,
-#     "new balance": 1000.0
-#   }
